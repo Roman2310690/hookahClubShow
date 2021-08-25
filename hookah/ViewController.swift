8f7bb261d16902253aa5b52ffa9da7edef255720
Роман Зиновец <r.zinovets1962@gmail.com>
2021-08-25 13:12:33 +0300

    ADDED FUNCTIONALITY

--- hookah/ViewController.swift
+++ hookah/ViewController.swift
@@ -9,7 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var progressBar: UIProgressView!
+    var timer = Timer()
+    var secondsPassed = 0
+    var totalTime = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        var tagButton = sender.tag
+        timer.invalidate()
+        
+        totalTime = tagButton
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerAction() {
+        
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed)/Float(totalTime)
+            print(Float(secondsPassed)/Float(totalTime))
+        }
+        
     }
 }
+   
